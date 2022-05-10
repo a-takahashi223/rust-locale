50f31d1e8fc7655b7bd3727acc0b769e19e47ad9
Akira Takahashi <a-takahashi@iij.ad.jp>
2022-05-10 18:23:03 +0000

    toupper

--- c/src/ctype.c
+++ c/src/ctype.c
@@ -1,6 +1,7 @@
 #include <inttypes.h>
 #include <locale.h>
 #include <string.h>
+#include <unistd.h>
 #include <wchar.h>
 #include <wctype.h>
 
@@ -30,6 +31,29 @@ utf8towc(wchar_t* wcbuf, const char* utf8_bytes, size_t length)
     return ret;
 }
 
+ssize_t
+wctoutf8(char* utf8_bytes, wchar_t wc)
+{
+    const locale_t l = newlocale(LC_CTYPE_MASK, "C.UTF-8", 0);
+    if (!l) {
+        return -0x1;
+    }
+    (void)uselocale(l);
+
+    ssize_t ret = 0;
+
+    mbstate_t state;
+    (void)memset(&state, 0, sizeof state);
+    ret = wcrtomb(utf8_bytes, wc, &state);
+    if (ret <= 0) {
+        ret = -0x2;
+    }
+
+    (void)uselocale(LC_GLOBAL_LOCALE);
+    freelocale(l);
+    return ret;
+}
+
 int_fast8_t
 iswspace_native(wint_t ch)
 {
@@ -44,4 +68,17 @@ iswspace_native(wint_t ch)
     (void)uselocale(LC_GLOBAL_LOCALE);
     freelocale(l);
     return ret ? 1 : 0;
+}
+
+wint_t
+towupper_native(wint_t ch)
+{
+    const locale_t l = newlocale(LC_CTYPE_MASK, "", 0);
+    (void)uselocale(l);
+
+    const wint_t ret = towupper(ch);
+
+    (void)uselocale(LC_GLOBAL_LOCALE);
+    freelocale(l);
+    return ret;
 }
--- src/ctype.rs
+++ src/ctype.rs
@@ -3,6 +3,9 @@ use libc::{c_char, wchar_t};
 use super::util::errno;
 
 mod c {
+    #[allow(non_camel_case_types)]
+    type wint_t = i64;
+
     #[link(name = "rustlocale", kind = "static")]
     extern "C" {
         pub fn utf8towc(
@@ -10,7 +13,9 @@ mod c {
             multibytes: *const libc::c_char,
             byte_length: libc::size_t,
         ) -> u8;
-        pub fn iswspace_native(ch: i64) -> i8;
+        pub fn wctoutf8(utf8_bytes: *mut libc::c_char, wc: libc::wchar_t) -> libc::ssize_t;
+        pub fn iswspace_native(ch: wint_t) -> i8;
+        pub fn towupper_native(ch: wint_t) -> wint_t;
     }
 }
 
@@ -35,20 +40,56 @@ pub trait CType {
     /// assert!('\u{2003}'.is_space());
     /// ```
     fn is_space(&self) -> bool;
+
+    /// Converts `self` to uppercase listed in the current locale.
+    ///
+    /// If no uppercase version is listed in the current locale, returns unmodified `self`.
+    ///
+    /// Only 1:1 character mapping can be performed by this function, e.g. the uppercase form of 'ß' is (with some exceptions)
+    /// the two-character string "SS", which cannot be obtained.
+    ///
+    /// # examples
+    ///
+    /// ```
+    /// use rust_locale::CType;
+    ///
+    /// assert_eq!(CType::to_uppercase(&'a'), 'A');
+    /// assert_eq!(CType::to_uppercase(&'1'), '1');
+    /// std::env::set_var("LANG", "POSIX");
+    /// assert_eq!(CType::to_uppercase(&'\u{017F}'), '\u{017F}');
+    /// std::env::set_var("LANG", "en_US");
+    /// assert_eq!(CType::to_uppercase(&'\u{017F}'), 'S');
+    /// assert_eq!(CType::to_uppercase(&'i'), 'I');
+    /// std::env::set_var("LANG", "tr_TR");
+    /// assert_eq!(CType::to_uppercase(&'i'), '\u{0130}');
+    /// ```
+    fn to_uppercase(&self) -> Self;
 }
 
 impl CType for char {
     fn is_space(&self) -> bool {
-        let length = self.len_utf8();
-        let mut buf = vec![0; length];
-        self.encode_utf8(&mut buf);
-        if length == 1 {
+        let buf = utf8_bytes(self);
+        if buf.len() == 1 {
             unsafe { libc::isspace(buf[0].into()) != 0 }
         } else {
             let wc = utf8towc(&buf);
             iswspace(wc)
         }
     }
+
+    fn to_uppercase(&self) -> char {
+        let bytes = utf8_bytes(self);
+        let wc = utf8towc(&bytes);
+        let upper = toupper(wc);
+        wctochar(upper)
+    }
+}
+
+fn utf8_bytes(c: &char) -> Vec<u8> {
+    let length = c.len_utf8();
+    let mut buf = vec![0; length];
+    c.encode_utf8(&mut buf);
+    buf
 }
 
 fn utf8towc(utf8_bytes: &Vec<u8>) -> wchar_t {
@@ -65,11 +106,30 @@ fn utf8towc(utf8_bytes: &Vec<u8>) -> wchar_t {
     }
 }
 
+fn wctochar(wc: wchar_t) -> char {
+    let mut buf = [0; 4];
+    match unsafe { c::wctoutf8(buf.as_mut_ptr(), wc) } {
+        length if length > 0 => {
+            let length = length as usize;
+            String::from_utf8(buf[..length].iter().map(|c| *c as u8).collect())
+                .unwrap()
+                .chars()
+                .next()
+                .unwrap()
+        }
+        status => panic!("wctochar failed. status={}, errno={}", status, errno()),
+    }
+}
+
 fn iswspace(wc: wchar_t) -> bool {
     match unsafe { c::iswspace_native(wc.into()) } {
         s if s >= 0 => s != 0,
         _ => panic!("iswspace_native failed. errno={}", errno()),
     }
+}
+
+fn toupper(wc: wchar_t) -> wchar_t {
+    unsafe { c::towupper_native(wc.into()) as wchar_t }
 }
 
 #[cfg(test)]
@@ -117,5 +177,18 @@ mod tests {
         assert!(!'\u{1361}'.is_space());
         environ::set_var("LANG", "am_ET");
         assert!('\u{1361}'.is_space());
+    }
+
+    #[test]
+    fn to_uppercase() {
+        assert_eq!(CType::to_uppercase(&'a'), 'A');
+        assert_eq!(CType::to_uppercase(&'1'), '1');
+        std::env::set_var("LANG", "POSIX");
+        assert_eq!(CType::to_uppercase(&'\u{017F}'), '\u{017F}');
+        std::env::set_var("LANG", "en_US");
+        assert_eq!(CType::to_uppercase(&'\u{017F}'), 'S');
+        assert_eq!(CType::to_uppercase(&'i'), 'I');
+        std::env::set_var("LANG", "tr_TR");
+        assert_eq!(CType::to_uppercase(&'i'), '\u{0130}');
     }
 }
