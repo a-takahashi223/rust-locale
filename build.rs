a74b22bddb473285d210dbf354b00fd8ff02aa1c
Akira Takahashi <a-takahashi@iij.ad.jp>
2022-05-15 14:17:55 +0900

    static link to libgnu

--- build.rs
+++ build.rs
@@ -10,6 +10,6 @@ fn main() {
     Command::new("make").args(["-C", "c"]).status().unwrap();
     println!(r"cargo:rustc-link-search=c/src");
     println!(r"cargo:rustc-link-search=c/lib");
-    println!(r"cargo:rustc-link-lib=gnu");
+    println!(r"cargo:rustc-link-lib=static=gnu");
     println!(r"cargo:rerun-if-changed=c/src/ctype.c");
 }
